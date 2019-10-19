dc97bfa31727176328991fb78252aaad44025a74
Chris Michael <chris.perez@upr.edu>
2019-10-19 12:27:59 -0400

    The problem of watching the videos by server has been corrected.

--- src/api/api.js
+++ src/api/api.js
@@ -35,11 +35,12 @@ async function getAnimeVideoByServer(id, chapter, serverNumber) {
     // There is a script on the page that will load the iframe dynamically
     // Here we find the script and then request the iframe URL directly
     if ((contents || '').includes('var video = [];')) {
-        const videoPageURL = contents.split('video[1] = \'<iframe class="player_conte" src="')[1].split('"')[0];
+        const videoPageURL = contents.split(`video[${serverNumber}] = \'<iframe class="player_conte" src="`)[1].split('"')[0];
         return getVideoURL(videoPageURL);
     }
   }
 }
+
 
 async function getVideoURL(url) {
   // This requests the underlying iframe page
@@ -49,7 +50,7 @@ async function getVideoURL(url) {
   if(video.length){
     // Sometimes the video is directly embedded
     const src = $(video).find('source').attr('src');
-    return src;
+    return src || `Video not available on the server`
   }
   else{
     // If the video is not embedded, there is obfuscated code that will create a video element
@@ -63,7 +64,7 @@ async function getVideoURL(url) {
     // Kind of dangerous, but the code is very obfuscated so its hard to tell how it decrypts the URL
     eval($script2);
     // The code above sets a variable called ss that is the mp4 URL
-    return l.ss;
+    return l.ss || `Video not available on the server`
   }
 }
 
