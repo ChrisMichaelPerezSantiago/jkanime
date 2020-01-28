8c05f5571bea3f139ac91f0ad61dd46e952d1d81
Chris Michael <chris.perez@upr.edu>
2020-01-28 11:59:47 -0400

    entries and author properties added to 'src/api/index.js'

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,20 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏'
+    message: 'jkanime API - 👋🌎🌍🌏',
+    author: 'Chris Michael',
+    entries: [
+      {
+        'latestAnimeAdded': '/api/v1/latestAnimes',
+        'getAnimeOvas': '/api/v1/ovas/[:page]',
+        'getAnimeMovies': '/api/v1/movies/[:page]',
+        'getAnimesByGender': '/api/v1/genres/[:genre]/[:page]',
+        'getAnimesListByLetter': '/api/v1/letter/[:letter]/[:page]',
+        'searchAnime': '/api/v1/search/[:title]',
+        'getAnimeVideoByServer': '/api/v1/video/[:id]/[:chapter]',
+        'schedule': '/api/v1/schedule/[:day]'
+      }
+    ]  
   });
 });
 
