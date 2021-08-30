414578df6187a0fff5f7841bd88aea1166f545fa
Douglas Brunal <douglas.brunal@servinformacion.com>
2021-08-30 09:15:57 -0500

    added genres and fixed [latestAnimes, Search]

--- src/api/index.js
+++ src/api/index.js
@@ -12,13 +12,15 @@ router.get('/', (req, res) => {
         'latestAnimeAdded': '/api/v1/latestAnimes',
         'getAnimeOvas': '/api/v1/ovas/[:page]',
         'getAnimeMovies': '/api/v1/movies/[:page]',
+        'getGenres': '/api/v1/genres',
         'getAnimesByGender': '/api/v1/genres/[:genre]/[:page]',
         'getAnimesListByLetter': '/api/v1/letter/[:letter]/[:page]',
         'searchAnime': '/api/v1/search/[:title]',
         'getAnimeVideoByServer': '/api/v1/video/[:id]/[:chapter]',
+        'getMedia': '/api/v1/media/[:id1]/[:id2]/[:id3]/',
         'schedule': '/api/v1/schedule/[:day]'
       }
-    ]  
+    ]
   });
 });
 
